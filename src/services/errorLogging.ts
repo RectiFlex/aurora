2cdcb168c3fde8bb926c5fdd38972e1b9345478b
Jace Lander <jace.lander@outlook.com>
2024-12-02 16:58:33 +1030

    Update errorLogging.ts

--- src/services/errorLogging.ts
+++ src/services/errorLogging.ts
@@ -1,13 +1,9 @@
 import { init, captureException } from '@sentry/react'
-import { Integrations, Intergration } from '@sentry/tracing'
 
 export const initErrorLogging = () => {
   if (import.meta.env.PROD) {
     init({
       dsn: import.meta.env.VITE_SENTRY_DSN,
-      integrations: [
-        new Integrations.BrowserTracing() as unknown as Integration
-      ],
       tracesSampleRate: 1.0,
       enabled: import.meta.env.PROD,
     })
