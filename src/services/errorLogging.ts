2843aacab95d8c9e92357d8348e34a429f15be2b
Jace Lander <jace.lander@outlook.com>
2024-12-02 16:01:17 +1030

    Update errorLogging.ts

--- src/services/errorLogging.ts
+++ src/services/errorLogging.ts
@@ -5,7 +5,7 @@ export const initErrorLogging = () => {
     Sentry.init({
       dsn: import.meta.env.VITE_SENTRY_DSN,
       integrations: [
-        new Sentry.BrowserTracing(),
+        new Sentry.Integrations.BrowserTracing(),
       ],
       tracesSampleRate: 1.0,
       enabled: import.meta.env.PROD,
